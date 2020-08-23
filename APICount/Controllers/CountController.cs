7a7e149d4abc96b4e5b2592f4d4dd4f1fd555873
Allan Oliveira <allanrj@gmail.com>
2020-08-23 18:25:06 +0100

    Commit Sample API

---
+++ APICount/Controllers/CountController.cs
@@ -0,0 +1,42 @@
+﻿namespace APICount.Controllers
+{
+    using APICount.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CountController : ControllerBase
+    {       
+        private readonly ILogger<CountController> _logger;
+        private static readonly Count _COUNT = new Count();
+        private readonly IConfiguration _configuration;
+
+        public CountController(ILogger<CountController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            lock (_COUNT)
+            {
+                _COUNT.Increment();
+                _logger.LogInformation($"Count - Actual value: {_COUNT.ActualValue}");
+
+                return new
+                {
+                    _COUNT.ActualValue,
+                    _COUNT.Local,
+                    _COUNT.Kernel,
+                    _COUNT.TargetFramework,
+                    MensagemFixa = "Test",
+                    MensagemVariavel = _configuration["VariableMessage"]
+                };
+            }
+        }
+    }
+}
---
+++ APICount/Models/Count.cs
@@ -0,0 +1,32 @@
+﻿namespace APICount.Models
+{
+    using System;
+    using System.Reflection;
+    using System.Runtime.Versioning;
+    public class Count
+    {
+        private static readonly string _LOCAL;
+        private static readonly string _KERNEL;
+        private static readonly string _TARGET_FRAMEWORK;
+
+        static Count()
+        {
+            _LOCAL = Environment.MachineName;
+            _KERNEL = Environment.OSVersion.VersionString;
+            _TARGET_FRAMEWORK = Assembly
+                .GetEntryAssembly()?
+                .GetCustomAttribute<TargetFrameworkAttribute>()?
+                .FrameworkName;
+        }
+
+        public int ActualValue { get; private set; } = 0;
+        public string Local { get => _LOCAL; }
+        public string Kernel { get => _KERNEL; }
+        public string TargetFramework { get => _TARGET_FRAMEWORK; }
+
+        public void Increment()
+        {
+            ActualValue++;
+        }
+    }
+}
